c3a59d04882f7e5105a09871895e99fe5a2aa4e5
MuriloRohor <murilo.marquezini@gmail.com>
2023-12-13 20:26:25 -0300

    incluindo a rota RootRouter no modulo principal da aplicação

--- main.py
+++ main.py
@@ -2,10 +2,14 @@ from fastapi import FastAPI
 from fastapi.staticfiles import StaticFiles
 import uvicorn
 
+from routes.RootRouter import router as rootRouter
+
+
 app = FastAPI()
 
 app.mount(path="/static", app=StaticFiles(directory="static"), name="static")
 
+app.include_router(rootRouter)
 
 if __name__ == "__main__":
     uvicorn.run(app="main:app", reload=True)
