9930c08eb2d1da7e48dc16024b25195100bb62c6
MuriloRohor <murilo.marquezini@gmail.com>
2023-12-13 20:20:23 -0300

    create RootRouter.py
    rota para renderizar o template index.html

---
+++ routes/RootRouter.py
@@ -0,0 +1,16 @@
+from fastapi import APIRouter, Request
+from fastapi.responses import HTMLResponse
+from fastapi.templating import Jinja2Templates
+
+router = APIRouter()
+
+templates = Jinja2Templates(directory="templates")
+
+@router.get("/", response_class=HTMLResponse)
+async def get_root(
+ request: Request,
+):
+ return templates.TemplateResponse(
+ "root/index.html",
+ {"request": request},
+ )
