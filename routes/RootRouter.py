b4bf273869e35b67adebeac80937ea92d9476ab0
MuriloRohor <murilo.marquezini@gmail.com>
2023-12-13 21:56:46 -0300

    Update RootRouter.py
    adicionando a rota para login em RootRouter

--- routes/RootRouter.py
+++ routes/RootRouter.py
@@ -1,6 +1,9 @@
-from fastapi import APIRouter, Request
+from fastapi import APIRouter, Depends, Request
 from fastapi.responses import HTMLResponse
 from fastapi.templating import Jinja2Templates
+
+from models.Usuario import Usuario
+from util.security import obter_usuario_logado
 
 router = APIRouter()
 
@@ -8,9 +11,21 @@ templates = Jinja2Templates(directory="templates")
 
 @router.get("/", response_class=HTMLResponse)
 async def get_root(
- request: Request,
+    request: Request,
 ):
- return templates.TemplateResponse(
- "root/index.html",
- {"request": request},
- )
+    return templates.TemplateResponse(
+        "root/index.html",
+        {"request": request},
+        )
+
+
+
+@router.get("/login", response_class=HTMLResponse)
+async def get_login(
+    request: Request,
+    usuario: Usuario = Depends(obter_usuario_logado),
+):
+    return templates.TemplateResponse(
+        "root/login.html",
+        {"request": request, "usuario": usuario},
+        )
