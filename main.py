61c1172c889b51ea99c2a00e30f18b6843fa3f46
MuriloRohor <murilo.marquezini@gmail.com>
2023-12-13 22:05:10 -0300

    Update main.py

--- main.py
+++ main.py
@@ -4,8 +4,16 @@ import uvicorn
 
 from routes.RootRouter import router as rootRouter
 
+from util.security import atualizar_cookie_autenticacao
+
+from repositories.UsuarioRepo import UsuarioRepo
+
+UsuarioRepo.criar_tabela()
+UsuarioRepo.criar_administrador_padrao()
+UsuarioRepo.criar_usuario_padrao()
 
 app = FastAPI()
+app.middleware("http")(atualizar_cookie_autenticacao)
 
 app.mount(path="/static", app=StaticFiles(directory="static"), name="static")
 
