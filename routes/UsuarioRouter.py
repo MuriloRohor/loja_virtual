fdea968ad203ec73a61aaa3ee73e8e99cb03a729
MuriloRohor <murilo.marquezini@gmail.com>
2023-12-14 00:43:28 -0300

    mostrar template para excluir usuario
    rota e template configurado.
    ao clicar no botão de excluir o usuário, renderiza a confirmação para exclusão

--- routes/UsuarioRouter.py
+++ routes/UsuarioRouter.py
@@ -2,6 +2,7 @@ from fastapi import (
  APIRouter,
  Depends,
  HTTPException,
+ Path,
  Request,
  status,
 )
@@ -30,3 +31,20 @@ async def get_index(
         "usuario/index.html",
         {"request": request, "usuario": usuario, "usuarios": usuarios},
     )
+    
+
+@router.get("/excluir/{id_usuario:int}", response_class=HTMLResponse)
+async def get_excluir(
+    request: Request,
+    id_usuario: int = Path(),
+    usuario: Usuario = Depends(obter_usuario_logado),
+):
+    if not usuario:
+        raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED)
+    if not usuario.admin:
+        raise HTTPException(status_code=status.HTTP_403_FORBIDDEN)
+    
+    usuario_excluir = UsuarioRepo.obter_por_id(id_usuario)
+    return templates.TemplateResponse(
+        "usuario/excluir.html",
+        {"request": request, "usuario": usuario, "usuario_excluir": usuario_excluir})
