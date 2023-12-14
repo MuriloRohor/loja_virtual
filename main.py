abca608173cf6814ee6889768d2be6b51de39dea
MuriloRohor <murilo.marquezini@gmail.com>
2023-12-14 01:07:15 -0300

    add template produto

--- main.py
+++ main.py
@@ -3,11 +3,13 @@ from fastapi.staticfiles import StaticFiles
 import uvicorn
 
 from routes.RootRouter import router as rootRouter
-from routes.UsuarioRouter import router as usuarioRouter 
+from routes.UsuarioRouter import router as usuarioRouter
+from routes.ProdutoRouter import router as produtoRouter
 
 from util.security import atualizar_cookie_autenticacao
 
 from repositories.UsuarioRepo import UsuarioRepo
+from repositories.ProdutoRepo import ProdutoRepo
 
 UsuarioRepo.criar_tabela()
 UsuarioRepo.criar_administrador_padrao()
@@ -21,6 +23,7 @@ app.mount(path="/static", app=StaticFiles(directory="static"), name="static")
 
 app.include_router(rootRouter)
 app.include_router(usuarioRouter)
+app.include_router(produtoRouter)
 
 
 if __name__ == "__main__":
